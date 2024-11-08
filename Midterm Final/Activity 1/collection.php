c2e5bb5a131a203e9825a8ed390c59e505c679f6
Kent Edoloverio <kentedoloverio25@gmail.com>
2024-11-08 14:01:20 +0800

    Update collection.php

--- Midterm Final/Activity 1/collection.php
+++ Midterm Final/Activity 1/collection.php
@@ -5,10 +5,9 @@ if (isset($_POST['num1']) && isset($_POST['num2'])) {
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
 
-    // Instantiate the Addition class
-    $addition = new Addition($num1, $num2);
+    $addition = new Addition(); 
+    $addition->setNumbers($num1, $num2); 
 
-    // Get the result of the addition
     $result = $addition->calculateSum();
     echo "<h3>Result of Adding {$num1} and {$num2} is: $result</h3>";
 } else {
